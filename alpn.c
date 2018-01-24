b8b85d45e0d1ded3181a11c6db468ec64dd01d8a
Joshua Jensch <jenschjoshua@gmail.com>
2018-01-25 00:16:44 +0100

    cleanup warnings

--- alpn.c
+++ alpn.c
@@ -7,19 +7,20 @@ int proto_cb(SSL *ssl,
              unsigned char *outlen,
              const unsigned char *in,
              unsigned int inlen,
-             void **arg)
+             void *arg)
 {
-  size_t num_supported = *(size_t*)arg[1];
-  const char **supported = arg[0];
+  void **args = (void**) arg;
+  size_t num_supported = *(size_t *)args[1];
+  const unsigned char **supported = (const unsigned char**)args[0];
 
   for (unsigned i = 0; i < inlen;)
   {
-    char protolen = in[i++];
-    char *proto = in + i;
-    
-    for(int isup=0;isup<num_supported;isup++)
+    unsigned char protolen = in[i++];
+    const unsigned char *proto = in + i;
+
+    for (size_t isup = 0; isup < num_supported; isup++)
     {
-      if(strncmp(supported[isup], proto, protolen) == 0)
+      if (memcmp(supported[isup], proto, protolen) == 0)
       {
         printf("PROTO MATCH: %.*s\n", protolen, proto);
         *out = proto;
@@ -32,7 +33,7 @@ int proto_cb(SSL *ssl,
   return SSL_TLSEXT_ERR_ALERT_FATAL;
 }
 
-void server_use_protos(SSL *ctx, const char **protos, const size_t len)
+void server_use_protos(SSL_CTX *ctx, const unsigned char **protos, const size_t len)
 {
   void **args = malloc(sizeof(void *) * 2);
   size_t *_len = malloc(sizeof(size_t *));
@@ -40,8 +41,6 @@ void server_use_protos(SSL *ctx, const char **protos, const size_t len)
   args[0] = protos;
   args[1] = _len;
 
-  printf("%s(%u)\n\n", *protos, len);
   printf("**DEBUG** Setting SSL alpn callback for ssl=%p\n", ctx);
-  SSL_CTX_set_alpn_protos(ctx, "\2h2", 3);
   SSL_CTX_set_alpn_select_cb(ctx, proto_cb, args);
 }
